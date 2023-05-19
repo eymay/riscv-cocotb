573fcc5849519fc1ea5b8911d1773dbe15232973
Eymen Ünay <20779470+Eymay@users.noreply.github.com>
2023-05-19 14:26:43 +0300

    Initial assembler-disassembler infra with a py test

--- add_cocotb.py
+++ add_cocotb.py
@@ -1,19 +1,37 @@
 import random
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import FallingEdge
+from cocotb.triggers import FallingEdge, Timer
 
+from as2hex import *
+
+instr = as2hex("add x1, x2, x4", "add")
 
 @cocotb.test()
-async def test_dff_simple(dut):
-    """ Test that d propagates to q """
+async def add_test(dut):
+    """Try accessing the design."""
+    clock = Clock(dut.clk, 10, units="ns")
+    cocotb.start_soon(clock.start())
+    
+    dut.rst = 0
+    await Timer(5, units="ns")
+    dut.rst = 1
+    await Timer(5, units="ns")
+    dut.dp.regfile.regs[2].value = 3
+    dut.dp.regfile.regs[4].value = 5
 
-    clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
-    cocotb.fork(clock.start())  # Start the clock
+    addr = 0
+    for i in range(4):
+        print(instr[i])
+        dut.instr_mem.imem[addr+i].value = int(instr[i], 16)
+        print(hex(dut.instr_mem.imem[addr+i].value))
 
-    await FallingEdge(dut.clk)  # Synchronize with the clock
-    for i in range(10):
-        val = random.randint(0, 1)
-        dut.d.value = val  # Assign the random value val to the input port d
-        await FallingEdge(dut.clk)
-        assert dut.q.value == val, "output q was incorrect on the {}th cycle".format(i)
+    
+    await Timer(5, units="ns")
+    await Timer(5, units="ns")
+    await Timer(5, units="ns")
+    await Timer(5, units="ns")
+
+    assert dut.dp.regfile.regs[1].value == 8, "{} != {}".format(dut.dp.regfile.regs[1].value, 8)
+
+
